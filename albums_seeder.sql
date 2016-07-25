d18ea1c3e573532d4d181c7bfdb1c2ca913a8879
Chris Davila <cmd1603@gmail.com>
2016-07-25 16:19:02 -0500

    saving missing comma to seeder exercise

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,9 +1,9 @@
 USE codeup_test_db;
 
-TRUNCATE albums;
-
-insert into albums (artist, name, release_date, sales, genre)
+INSERT INTO albums (artist, name, release_date, sales, genre)
 
 values('Drake', 'Views', '2016', '50', 'R&B'),
 	  ('Pink Floyd', 'The Dark Side of the Moon', '1973', '22', 'Progressive Rock')	,
-	  ('Bee Gees', 'Saturday Night Fever', '1977', '19', 'Disco');
+	  ('Bee Gees', 'Saturday Night Fever', '1977', '19', 'Disco'),
+	  ('Led Zeppelin', 'Led Zeppelin IV', '1971', '29', 'Hard Rock, Heavy Metal'),
+      ('Shania Twain', 'Come On Over', '1997', '29', 'Country, Pop');
