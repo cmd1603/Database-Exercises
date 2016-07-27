08f7bdb0f0a969606f637b0186d00f6df12ccb75
Chris Davila <cmd1603@gmail.com>
2016-07-27 10:04:13 -0500

    saving additions to where exercise-using AND/OR options

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,13 +1,25 @@
 -- All employees with first name Irena, Vidya, and Maya
 SELECT emp_no, first_name
 From employees
-Where first_name IN ('Irena', 'Vidya', 'Maya');
+Where (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') ORDER BY first_name ASC;
+
+-- Now add a condition to find everybody with those names who is also male
+SELECT emp_no, first_name
+From employees 
+Where (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
+
 
 -- Employees whose last name start with E or end with E
-SELECT last_name
+SELECT emp_no, first_name, last_name
 From employees
 Where last_name LIKE 'E%'
 	OR last_name LIKE '%E';
+	
+-- Employees whose last name start with E and ends with E
+SELECT emp_no, first_name, last_name
+From employees
+Where last_name LIKE 'E%'
+	AND last_name LIKE '%E';	
 
 -- Employees who were hired in the 90s and have their birthday on xmas.
 SELECT hire_date
