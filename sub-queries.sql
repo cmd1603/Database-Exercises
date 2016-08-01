f30f45661057fb2cfa613a63c042a42cd62993bc
Chris Davila <cmd1603@gmail.com>
2016-08-01 13:52:42 -0500

    Saving sub-queries exercise

--- sub-queries.sql
+++ sub-queries.sql
@@ -1,1 +1,38 @@
-SELECT first_name, last_name
+-- Find all the employees with the same hire date as employee 101010 using a sub-query.
+SELECT CONCAT(first_name, ' ', last_name) AS 'Name', hire_date AS 'Hire Date'
+FROM employees 
+WHERE hire_date IN (
+	SELECT hire_date 
+	FROM employees
+	WHERE emp_no = 101010
+);
+
+-- Find all the titles held by all employees with the first name Aamod
+SELECT title AS 'Title' 
+FROM titles 
+WHERE emp_no IN (
+	SELECT emp_no 
+	FROM employees
+	WHERE first_name = 'Aamod'
+);
+
+-- Find all the department managers that are female.
+SELECT CONCAT(first_name, ' ', last_name) AS 'Female Managers'
+FROM employees 
+WHERE gender = 'F' AND emp_no IN (
+	SELECT emp_no 
+	FROM dept_manager
+);
+
+-- BONUS Find all the department names that have female managers.
+SELECT dept_name AS 'Department Names'
+FROM departments
+WHERE dept_no IN (
+	SELECT dept_no
+	FROM dept_manager
+	WHERE emp_no IN(
+		SELECT emp_no
+		FROM employees
+		WHERE gender = 'F' AND > NOW()
+));
+
